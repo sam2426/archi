b5a1a6f82d3967fccb2c53d906c0f987c9f77117
Shubham Shaw <shubhamk.ss@smartdatainc.net>
2020-12-15 15:43:08 +0530

    added shared input components

--- src/app/pages/demo/pages/shared/shared.module.ts
+++ src/app/pages/demo/pages/shared/shared.module.ts
@@ -1,15 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { SharedRoutingModule } from './shared-routing.module';
 import { SharedComponent } from './shared.component';
+import { ButtonsModule, ControlsModule } from '@app/shared';
 
 
 @NgModule({
   declarations: [SharedComponent],
   imports: [
     CommonModule,
-    SharedRoutingModule
+    SharedRoutingModule,
+    ReactiveFormsModule,
+    ButtonsModule,
+    ControlsModule,
   ]
 })
 export class SharedModule { }
---
+++ src/app/shared/controls/input/input.component.ts
@@ -0,0 +1,57 @@
+import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+
+@Component({
+  selector: 'app-input',
+  templateUrl: './input.component.html',
+  styleUrls: ['./input.component.scss'],
+  providers: [
+    {
+      provide: NG_VALUE_ACCESSOR,
+      useExisting: forwardRef(()=>InputComponent),
+      multi: true
+    }
+  ]
+})
+export class InputComponent implements OnInit, ControlValueAccessor {
+
+  @Input() placeholder: string;
+  @Output() changed = new EventEmitter<string>(); //the datatype will be same as that of value property
+
+  value: string;
+  isDisabled: boolean;
+  private propagateChange: any=()=>{};
+  private propageteTouched: any=()=>{};
+
+  constructor() { }
+
+  writeValue(value: string): void {
+    this.value = value;
+  }
+
+  registerOnChange(fn: any): void {
+    this.propagateChange = fn;
+  }
+
+  registerOnTouched(fn: any): void {
+    this.propageteTouched = fn;
+  }
+
+  setDisabledState?(isDisabled: boolean): void {
+    this.isDisabled = isDisabled;
+  }
+
+  ngOnInit(): void {
+  }
+
+  onKeyup(value: string): void {
+    this.value = value;
+    this.propagateChange(value);
+    this.changed.emit(value);
+  }
+
+  onBlur(): void {
+    this.propageteTouched();
+  }
+
+}
